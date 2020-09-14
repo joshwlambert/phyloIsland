382832c515ec28634664066fbb3053ffed577ed1
joshwlambert <j.w.l.lambert@rug.nl>
2020-09-14 11:28:29 +0200

    added tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(phyloIslands)
+
+test_check("phyloIslands")
---
+++ tests/testthat/test-sim_diversity.R
@@ -0,0 +1,4 @@
+test_that("sim_diversity produces correct output", {
+  output <- sim_diversity()
+  expect_equal(output, "WIP")
+})
